73c6838dcdf000563730eacd62b637d9a96fdbdc
mpashuk <mariapashuk@gmail.com>
2019-03-27 15:20:32 -0400

    Config

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -4,15 +4,13 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by jt on 5/24/17.
- */
-@Service
-@Primary
-@Profile("de")
-public class PrimaryGermanGreetingService implements GreetingService {
+    public class PrimaryGermanGreetingService implements GreetingService {
 
-    private GreetingRepository greetingRepository;
+        private GreetingRepository greetingRepository ;
+
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
